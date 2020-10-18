a5030e7d7130f86d276a11b007f0c60fd95d51ab
pavelperc <pavelperc@mail.ru>
2020-10-19 00:30:09 +0300

    Added readme. Renamed cmake project.

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -17,6 +17,7 @@ int main() {
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    /* Need for mac os */
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
